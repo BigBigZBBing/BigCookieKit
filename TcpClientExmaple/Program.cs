1cfd09fcbc6c0cc77ae47d12e1a22361231a8769
张炳彬 <zbb5896@163.com>
2021-03-02 01:13:49 +0800

    TCP通信组件简单演示

---
+++ TcpClientExmaple/Program.cs
@@ -0,0 +1,36 @@
+﻿using BigCookieKit.Communication;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace TcpClientExmaple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 7447);
+
+            client.OnConnect = user =>
+            {
+                Random random = new Random();
+                while (true)
+                {
+                    byte[] message = Encoding.UTF8.GetBytes("测试数据" + random.Next(short.MaxValue));
+                    user.SendMessage(message);
+                    Thread.Sleep(1);
+                }
+            };
+
+            client.OnCallBack = (user, packet) =>
+            {
+
+            };
+
+            client.StartConnect();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpServerExmaple/Program.cs
@@ -0,0 +1,33 @@
+﻿using BigCookieKit.Communication;
+using System;
+using System.Text;
+
+namespace TcpServerExmaple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpServer tcpServer = new TcpServer(7447);
+
+            tcpServer.OnConnect = user =>
+            {
+                Console.WriteLine($"{user.UserHost}:{user.UserPort}接入~");
+            };
+
+            tcpServer.OnReceive = (user, packet) =>
+            {
+                Console.WriteLine($"[{user.UserHost}:{user.UserPort}]:{Encoding.UTF8.GetString(packet)}");
+            };
+
+            tcpServer.OnExit = user =>
+            {
+                Console.WriteLine($"{user.UserHost}:{user.UserPort}离开~");
+            };
+
+            tcpServer.StartServer();
+
+            Console.ReadLine();
+        }
+    }
+}
